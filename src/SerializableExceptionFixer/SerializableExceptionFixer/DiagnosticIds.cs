dbcf521677f211f2fdb5682cd0fdcd5e0e5691e1
David Störmer <badf00d@posteo.de>
2018-02-22 18:43:06 +0100

    Changed some diagnostic ids.

--- src/SerializableExceptionFixer/SerializableExceptionFixer/DiagnosticIds.cs
+++ src/SerializableExceptionFixer/SerializableExceptionFixer/DiagnosticIds.cs
@@ -3,10 +3,12 @@
     public static class DiagnosticIds
     {
         public const string SerializableExceptionAttributeMissing = "SE1010";
-        public const string ParameterlessConstructorMissing = "SE1020";
-        public const string ConstructorThatAcceptsStringMissing = "SE1030";
-        public const string ConstructorThatAcceptsStringAndExceptionMissing = "SE1040";
-        public const string ConstructorThatAcceptsSerializationInfoAndStreamingContextMissing = "SE1050";
-        public const string ExceptionsShouldEndWithExceptions = "SE2050";
+        public const string ConstructorThatAcceptsSerializationInfoAndStreamingContextMissing = "SE1020";
+
+        public const string ParameterlessConstructorMissing = "SE2010";
+        public const string ConstructorThatAcceptsStringMissing = "SE2020";
+        public const string ConstructorThatAcceptsStringAndExceptionMissing = "SE2030";
+        
+        public const string ExceptionsShouldEndWithExceptions = "SE3010";
     }
 }
